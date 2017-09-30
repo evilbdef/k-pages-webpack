41087519ddcf6d1a32f1cbdd2ac0075373d86784
faimaklg <faimaklg@163.com>
2017-09-30 15:44:06 +0800

    模块热更新

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-// const webpack = require('webpack')
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const baseWebpackConfig = require('./webpack.base.config')
@@ -45,6 +45,7 @@ module.exports = merge(baseWebpackConfig, {
     ]
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new ExtractTextPlugin({
       filename: `${config.staticPath}/css/[name].css`
     })
